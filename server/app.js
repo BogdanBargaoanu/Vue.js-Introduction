257a400fa0a1fad0e04620b11a6d4c67209c4850
BogdanBargaoanu <bn19bba@yahoo.com>
2024-07-30 18:11:14 +0300

    mysqlconnection

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sql = require('mssql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -27,31 +26,85 @@ const options = {
 };
 
 const openapiSpecification = swaggerJsdoc(options);
+app.use('/api', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
-
-// SQL Server setup
-const config = {
-  /*user: 'your_username', // replace with your username
-  password: 'your_password', // replace with your password
-  server: 'localhost', // use 'localhost\\SQLEXPRESS' if you have a named instance
-  database: 'your_database', // replace with your database name*/
-  server: 'localhost\\MSSQLLocalDB',
-  options: {
-    encrypt: true, // for Azure
-    trustServerCertificate: true, // change to true for local dev / self-signed certs
-    trustedConnection: true // Use Windows Authentication
-  }
-};
-
-sql.connect(config).then(pool => {
-  if (pool.connected) {
-    console.log('Connected to SQL Server');
-  }
-}).catch(err => {
-  console.error('Database connection failed: ', err);
+// MySQL setup
+const mysql = require('mysql');
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'c4shfl0w4pp',
+  database: 'cashflowapp'
 });
 
-app.use('/api', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log('Connected to database');
+    const createUsersTableQuery = `CREATE TABLE IF NOT EXISTS users (
+      idUsers INT NOT NULL AUTO_INCREMENT,
+      username VARCHAR(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+      password VARCHAR(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+      PRIMARY KEY (idUsers)
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci`;
+    const createEntitiesTableQuery = `CREATE TABLE IF NOT EXISTS entities (
+      idEntities INT NOT NULL AUTO_INCREMENT,
+      name VARCHAR(100) NULL,
+      isUser TINYINT(1) NULL,
+      PRIMARY KEY (idEntities))`;
+    const createCashflowLogTableQuery = `CREATE TABLE IF NOT EXISTS cashflowLog (
+      idcashflowLog INT NOT NULL AUTO_INCREMENT,
+      idUser INT NOT NULL,
+      description VARCHAR(500) NULL,
+      idEntity INT NOT NULL,
+      type VARCHAR(45) NULL,
+      value FLOAT NOT NULL,
+      currency VARCHAR(45) NOT NULL,
+      date DATETIME NOT NULL,
+      PRIMARY KEY (idcashflowLog),
+      INDEX FK_Users_idx (idUser ASC) VISIBLE,
+      INDEX FK_Entities_idx (idEntity ASC) VISIBLE,
+      CONSTRAINT FK_Users
+        FOREIGN KEY (idUser)
+        REFERENCES cashflowapp.users (idUsers)
+        ON DELETE RESTRICT
+        ON UPDATE NO ACTION,
+      CONSTRAINT FK_Entities
+        FOREIGN KEY (idEntity)
+        REFERENCES cashflowapp.entities (idEntities)
+        ON DELETE RESTRICT
+        ON UPDATE NO ACTION)`
+    db.query(createUsersTableQuery, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log('Users table checked/created successfully.');
+      }
+    });
+    db.query(createEntitiesTableQuery, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log('Entities table checked/created successfully.');
+      }
+    })
+    db.query(createCashflowLogTableQuery, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log('Cashflow log table checked/created successfully.');
+      }
+    })
+  }
+});
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
