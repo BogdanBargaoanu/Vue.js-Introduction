a245f202d3fc35e9069919b6d6f381581422a2e2
BogdanBargaoanu <bn19bba@yahoo.com>
2024-08-11 23:02:05 +0300

    initial display

--- Cashflow/src/router/index.js
+++ Cashflow/src/router/index.js
@@ -1,29 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import UserLogin from '../components/UserLogin.vue'
+import UserLogin from '../components/UserLogin.vue';
+
+const routes = [
+  { path: '/', redirect: '/dashboard' },
+  { path: '/login', component: UserLogin }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }, 
-    {
-      path: '/login',
-      name: 'login',
-      compoenent: UserLogin
-    }
-  ]
-})
+  history: createWebHistory(),
+  routes // short for `routes: routes`
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth) && !localStorage.getItem('user-token')) {
+    next('/login');
+  } else {
+    next();
+  }
+});
 
 export default router
